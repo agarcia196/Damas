1c9347a4d645bc511532af7ab33de467ca437b9c
Nicolas Perez <pereznicolas90@gmail.com>
2019-03-27 06:51:52 -0500

    Commit Inicial
    Subiendo lo que ya estaba hecho

---
+++ js/juego.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	for(i = 1; i<= 8; i++) {
+		x = (i % 2 == 0) ? 1 : 0;
+		$("#contenedor").append("<div id=row-" + i + " class='row'></div>")
+		for(j = 1; j<= 8; j++) {
+			if(j % 2 == x) {
+				col_id = 'row-' + i + '-col-' + j;
+				$("#row-" + i).append("<div class='col-white' data-row= "+i+" data-col="+j+" id=" + col_id + "></div>");
+				if( i <= 3 ) {
+					ficha_id = 'f1-' + 'row-' + i + '-col-' + j;
+					$("#"+ col_id).append("<div class='ficha1' id="+ ficha_id +"></div>");
+				}
+				if( i >= 6) {
+					ficha_id = 'f2-' + 'row-' + i + '-col-' + j;
+					$("#"+ col_id).append("<div class='ficha2' id="+ ficha_id +"></div>");
+				}
+			}
+			else{
+				$("#row-" + i).append("<div class='col-black'></div>");
+			}
+
+		}
+	}
+	//$(".ficha1").draggable({ revert: 'invalid' });
+	$(".ficha2").draggable({ revert:'invalid',opacity: 0.5,revertDuration: 1000,containment: $("#colcenter")});
+	var grid = $( ".ficha2" ).draggable( "option", "containment" );
+	$( "#row-1-col-2" ).droppable({
+  		accept: ".ficha2",
+  		drop:function(event, ui) {
+  				$( ".ficha1" ).toggle( "explode" );
+  		}
+
+	});
+
+	console.log(grid);
+	//disabled: true 
+});
